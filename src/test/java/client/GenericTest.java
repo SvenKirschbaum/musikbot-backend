5aa813a18907005fbfd065d6b331afd0ae661465
Sven Kirschbaum <sven@elite12.de>
2016-07-11 08:27:38 +0200

    Generic Test

---
+++ src/test/java/client/GenericTest.java
@@ -0,0 +1,14 @@
+package client;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GenericTest {
+
+	@Test
+	public final void test() {
+		assertTrue(0 == 2-2);
+	}
+
+}
