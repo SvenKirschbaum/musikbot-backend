54e9f2af155b0f0c3753b2d2901fa940e063bcfe
Sven Kirschbaum <sven@elite12.de>
2016-10-01 05:43:56 +0200

    Test-ENdpoint für Monitoring, wird anschließend entfernt

--- src/main/java/de/elite12/musikbot/server/rest/Status.java
+++ src/main/java/de/elite12/musikbot/server/rest/Status.java
@@ -13,6 +13,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -80,6 +81,15 @@ public class Status {
         st.playlistdauer = dauer / 60;
 
         return st;
+    }
+
+    @GET
+    @Path("/status")
+    public Response ConnectionStatus() {
+        if (Controller.getInstance().getState().equalsIgnoreCase("Keine Verbindung zum BOT")) {
+            return Response.serverError().build();
+        }
+        return Response.ok().entity(Controller.getInstance().getState()).build();
     }
 }
 
