5b7176d315aa555dcaaec2ecc8727681f624b7f0
Sven Kirschbaum <sven@elite12.de>
2017-01-10 09:34:33 +0100

    Fixed test

--- src/test/java/client/GenericTest.java
+++ src/test/java/client/GenericTest.java
@@ -4,20 +4,11 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-import com.wrapper.spotify.models.Track;
-
-import de.elite12.musikbot.shared.Util;
-
 public class GenericTest {
 
     @Test
     public final void test() {
         assertTrue(0 == 2 - 2);
-    }
-
-    public static void main(String... strings) {
-        Track t = Util.getTrackfromAlbum("7n3QJc7TBOxXtlYh4Ssll8", 1);
-        System.out.println(t.getName());
     }
 
 }
