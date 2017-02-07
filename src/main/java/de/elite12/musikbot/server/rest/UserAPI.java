9cd64bb52362f31260a41b80e0133736361a4fff
Sven Kirschbaum <sven@elite12.de>
2017-02-07 01:32:02 +0100

    Logging for UserAPI

--- src/main/java/de/elite12/musikbot/server/rest/UserAPI.java
+++ src/main/java/de/elite12/musikbot/server/rest/UserAPI.java
@@ -3,7 +3,6 @@ package de.elite12.musikbot.server.rest;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
@@ -23,83 +22,88 @@ import de.elite12.musikbot.shared.Util;
 
 @Path("/user")
 public class UserAPI {
-
-	@Context
-	HttpServletRequest req;
-	@Context
-	SecurityContext sc;
-
-	@POST
-	@Consumes(MediaType.TEXT_PLAIN)
-	@Path("{userid}/{attribute}")
-	public Response updateUser(@PathParam("userid") int userid, @PathParam("attribute") String attr, String value) {
-		User user = (User) sc.getUserPrincipal();
-		User target = Controller.getInstance().getUserservice().getUserbyId(userid);
-		switch (attr) {
-			case "email": {
-				if (selforadmin(user, target)) {
-					if (Util.isValidEmailAddress(value)) {
-						target.setEmail(value);
-						Controller.getInstance().getUserservice().changeUser(target);
-						return Response.status(200).build();
-					} else {
-						return Response.status(400).build();
-					}
-				} else {
-					return Response.status(401).build();
-				}
-			}
-			case "password": {
-				if (selforadmin(user, target)) {
-					target.setPassword(Controller.getInstance().getUserservice().hashPW(value));
-					Controller.getInstance().getUserservice().changeUser(target);
-					return Response.status(200).build();
-				} else {
-					return Response.status(401).build();
-				}
-			}
-			case "admin": {
-				if (user.isAdmin()) {
-					boolean admin = value.equalsIgnoreCase("ja") || value.equalsIgnoreCase("yes")
-							|| value.equalsIgnoreCase("true");
-					target.setAdmin(admin);
-					Controller.getInstance().getUserservice().changeUser(target);
-					return Response.status(200).build();
-				} else {
-					return Response.status(401).build();
-				}
-			}
-			case "username": {
-				if (user.isAdmin()) {
-					if (Controller.getInstance().getUserservice().getUserbyName(value) == null) {
-						try (
-								Connection c = Controller.getInstance().getDB();
-								PreparedStatement stmnt = c
-										.prepareStatement("UPDATE PLAYLIST SET AUTOR = ? WHERE AUTOR = ?");
-						) {
-							stmnt.setString(1, value);
-							stmnt.setString(2,target.getName());
-							stmnt.executeUpdate();
-							target.setName(value);
-							Controller.getInstance().getUserservice().changeUser(target);
-						}
-						catch (SQLException e) {
-							Logger.getLogger(this.getClass()).error("SQLException", e);
-						}
-						return Response.status(200).build();
-					}
-					else {
-						return Response.status(409).build();
-					}
-				}
-			}
-			default: {
-				return Response.status(404).entity("Userid ungültig").build();
-			}
-		}
-	}
-
-	private boolean selforadmin(User user, User target) {
-		return user.equals(target) || user.isAdmin();
-	}
+    
+    @Context
+    HttpServletRequest req;
+    @Context
+    SecurityContext sc;
+    
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Path("{userid}/{attribute}")
+    public Response updateUser(@PathParam("userid") int userid, @PathParam("attribute") String attr, String value) {
+        User user = (User) sc.getUserPrincipal();
+        User target = Controller.getInstance().getUserservice().getUserbyId(userid);
+        switch (attr) {
+            case "email": {
+                if (selforadmin(user, target)) {
+                    if (Util.isValidEmailAddress(value)) {
+                        target.setEmail(value);
+                        Controller.getInstance().getUserservice().changeUser(target);
+                        Logger.getLogger(UserAPI.class)
+                                .info(user + " changed Email-Address of " + target + "to " + target.getEmail());
+                        return Response.status(200).build();
+                    } else {
+                        return Response.status(400).build();
+                    }
+                } else {
+                    return Response.status(401).build();
+                }
+            }
+            case "password": {
+                if (selforadmin(user, target)) {
+                    target.setPassword(Controller.getInstance().getUserservice().hashPW(value));
+                    Controller.getInstance().getUserservice().changeUser(target);
+                    Logger.getLogger(UserAPI.class).info(user + " changed the Password of " + target);
+                    return Response.status(200).build();
+                } else {
+                    return Response.status(401).build();
+                }
+            }
+            case "admin": {
+                if (user.isAdmin()) {
+                    boolean admin = value.equalsIgnoreCase("ja") || value.equalsIgnoreCase("yes")
+                            || value.equalsIgnoreCase("true");
+                    target.setAdmin(admin);
+                    Controller.getInstance().getUserservice().changeUser(target);
+                    Logger.getLogger(UserAPI.class)
+                            .info(user + " changed the Admin-Status of " + target + " to " + target.isAdmin());
+                    return Response.status(200).build();
+                } else {
+                    return Response.status(401).build();
+                }
+            }
+            case "username": {
+                if (user.isAdmin()) {
+                    if (Controller.getInstance().getUserservice().getUserbyName(value) == null) {
+                        try (
+                                Connection c = Controller.getInstance().getDB();
+                                PreparedStatement stmnt = c
+                                        .prepareStatement("UPDATE PLAYLIST SET AUTOR = ? WHERE AUTOR = ?");
+                        ) {
+                            stmnt.setString(1, value);
+                            stmnt.setString(2, target.getName());
+                            stmnt.executeUpdate();
+                            Logger.getLogger(UserAPI.class)
+                                    .info(user + " changed the Username of " + target + " to " + value);
+                            target.setName(value);
+                            Controller.getInstance().getUserservice().changeUser(target);
+                        } catch (SQLException e) {
+                            Logger.getLogger(this.getClass()).error("SQLException", e);
+                        }
+                        return Response.status(200).build();
+                    } else {
+                        return Response.status(409).build();
+                    }
+                }
+            }
+            default: {
+                return Response.status(404).entity("Userid ungültig").build();
+            }
+        }
+    }
+    
+    private boolean selforadmin(User user, User target) {
+        return user.equals(target) || user.isAdmin();
+    }
 }
