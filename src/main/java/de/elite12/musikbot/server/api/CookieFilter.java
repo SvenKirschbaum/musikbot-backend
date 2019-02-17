5a96212850f0e29c4ed6520ad22dc85b1e3ba204
Sven Kirschbaum <sven@elite12.de>
2019-02-17 03:21:10 +0100

    Filter Cookie in Rest API

--- src/main/java/de/elite12/musikbot/server/api/CookieFilter.java
+++ src/main/java/de/elite12/musikbot/server/api/CookieFilter.java
@@ -11,14 +11,12 @@ import javax.ws.rs.ext.Provider;
 
 @Provider
 @Priority(Priorities.HEADER_DECORATOR)
-public class CorsFilter implements ContainerResponseFilter {
+public class CookieFilter implements ContainerResponseFilter {
 
 	@Override
 	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
 			throws IOException {
-		responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
-		responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
-		responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+		responseContext.getHeaders().remove("set-cookie");
 	}
 
 }
