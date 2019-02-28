0130614ecf1d2ac9a860f2339fae30170f72de49
Sven Kirschbaum <sven@elite12.de>
2019-02-28 02:45:12 +0100

    Playlist import view

--- src/main/java/de/elite12/musikbot/server/services/GapcloserService.java
+++ src/main/java/de/elite12/musikbot/server/services/GapcloserService.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import javax.annotation.PostConstruct;
