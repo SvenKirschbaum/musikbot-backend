0b681894aa90dbd650cd20381f8ab0ab19a4bdaf
Sven Kirschbaum <sven@elite12.de>
2019-03-30 17:28:16 +0100

    Changed build to war

--- src/main/java/de/elite12/musikbot/server/core/MusikbotApplication.java
+++ src/main/java/de/elite12/musikbot/server/core/MusikbotApplication.java
@@ -1,8 +1,11 @@
 package de.elite12.musikbot.server.core;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,10 +13,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan("de.elite12.musikbot.server")
 @EntityScan("de.elite12.musikbot.server.data.entity")
 @EnableJpaRepositories("de.elite12.musikbot.server.data.repository")
-public class MusikbotApplication {
+public class MusikbotApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MusikbotApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ApplicationRunner.class);
+    }
 
 }
