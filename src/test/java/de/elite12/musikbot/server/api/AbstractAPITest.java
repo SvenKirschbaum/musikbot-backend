72d2dd047737c92531a7eef62028e69311ded048
Sven Kirschbaum <sven@elite12.de>
2020-09-04 13:04:11 +0200

    Fixed VersionAPITest

--- src/test/java/de/elite12/musikbot/server/api/AbstractAPITest.java
+++ src/test/java/de/elite12/musikbot/server/api/AbstractAPITest.java
@@ -1,8 +1,10 @@
 package de.elite12.musikbot.server.api;
 
+import de.elite12.musikbot.server.services.JWTUserService;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -17,8 +19,11 @@ public abstract class AbstractAPITest {
 
     protected MockMvc mockMvc;
 
+    @MockBean
+    protected JWTUserService jwtUserService = null;
+
     @Before
-    public void abstract_setup(){
+    public void abstract_setup() {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context).build();
     }
 }
